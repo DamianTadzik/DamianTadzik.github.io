47f04fc26b73571037a7030d3af7ea2bcb731566
Damian Brzana <brzanad@gmail.com>
2025-07-13 02:27:52 +0200

    Added skeleton of the improvement, to be done...

---
+++ js/enhance_projects_list.js
@@ -0,0 +1,10 @@
+// I need to create enhanced file based on the basic file and all other JSONs
+// projects_list.json
+// |
+// | /- project_....json etc...
+// |/
+// * enhance_projects_list.js
+// |
+// V
+// enhanced_projects_list.json
+
